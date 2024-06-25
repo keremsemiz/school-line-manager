a3e7de456f4b1ca0359233c1826434aaf1b89c4f
Kerem Semiz <kerox350@gmail.com>
2024-06-25 23:23:04 +0200

    do lots of work, mainly css stuff

--- mensa-manager/src/components/Welcome.jsx
+++ mensa-manager/src/components/Welcome.jsx
@@ -1,12 +1,17 @@
 import React, { useRef, useEffect, useState } from 'react';
+import './Welcome.css'
 
 function Welcome(){
 
 
     return(
         <div classname="container">
-            <h1>Welcome to the NMS Cafeteria Manager</h1>
-            <h3>Please wait 5 seconds to get started</h3>
+            <div className="header">
+                <p className='headerText'>Made by Kerem & Vibbodh 10d</p>
+                <p className='headerText'>Wednesday, June 26th</p>
+            </div>
+            <h2 className='Title'>Welcome to the NMS Cafeteria Manager</h2>
+            <p>Please wait 5 seconds to get started</p>
         </div>
     );
 }
